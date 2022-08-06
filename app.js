25880c74152a554469c9560756c334ddde80e301
Promise Okeke <plastamasta21@gmail.com>
2022-08-06 13:06:18 +0100

    Done with the functionality of the toggle buttons functionality all that remains to do is to make sure that they don't disappear when it is out of focus

--- app.js
+++ app.js
@@ -2,10 +2,29 @@ const numbers = document.querySelectorAll(".number");
 const ioDisplay = document.querySelector("#ioDisplay");
 const resetDisplay = document.querySelector(".reset");
 const Delete = document.querySelector(".delete");
+const toggleBtn = document.querySelectorAll(".toggle-btn");
+const toggleSliderCase = document.querySelector(".toggle-slider-case");
+const cssFile = document.querySelector("#cssfile");
 
+// the toggle section
+
+toggleBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    changeTheme(btn.innerText);
+  });
+});
+function changeTheme(i) {
+  if (i == 1) {
+    cssFile.setAttribute("href", "default-theme1.css");
+  } else {
+    cssFile.setAttribute("href", `theme${i}.css`);
+  }
+}
+// toggle section ends
+
+// calculator
 numbers.forEach((number) => {
   number.addEventListener("click", () => {
-    // alert("nice");
     const numberValue = number.innerText;
     ioDisplay.value += numberValue;
   });
